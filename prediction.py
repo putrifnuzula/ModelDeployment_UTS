d3f828b9c8a6aaef7be1519223ab48df6147954d
putrifnuzula <168532862+putrifnuzula@users.noreply.github.com>
2025-01-14 02:08:56 +0700

    prediction.py

--- prediction.py
+++ prediction.py
@@ -27,7 +27,7 @@ def main():
     if st.button('Make Prediction'):
         features = [CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]
         # ft = feat_list = np.array(features, dtype=object)
-        result = make_prediction(ft)
+        result = make_prediction(features)
         st.success(f'The prediction is: {result}')
 
 
